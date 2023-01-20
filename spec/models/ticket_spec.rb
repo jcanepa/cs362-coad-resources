426d09de0fcc10264c78fcccbeed3ad8032d17b5
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-19 23:52:23 -0800

    Update ticket test with section comments.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,23 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  # Dependency objects creation
   let(:category) { ResourceCategory.new }
   let(:region) { Region.new }
-  let (:ticket) {
-    Ticket.create(
-      name: 'Foo',
-      phone: '+1234567890',
-      description: "Lorem ipsum",
-      region_id: region.id,
-      resource_category_id: category.id,
-      closed: false
-    )
+
+  let (:ticket) { Ticket.create(
+    name: 'Foo',
+    phone: '+1234567890',
+    description: "Lorem ipsum",
+    region_id: region.id,
+    resource_category_id: category.id,
+    closed: false)
   }
 
+  # Test instanciation
   it "exists" do
     Ticket.new
   end
 
+  # Test attributes
   it "has a name" do
     expect(ticket).to respond_to(:name)
   end
@@ -31,6 +33,7 @@ RSpec.describe Ticket, type: :model do
     expect(ticket).to respond_to(:description)
   end
 
+  # Foreign key attributes
   it "references a region" do
     expect(ticket).to respond_to(:region_id)
   end
@@ -43,11 +46,10 @@ RSpec.describe Ticket, type: :model do
     expect(ticket).to respond_to(:closed)
   end
 
+  # Test model associations (relationships)
   context 'associations' do
     it { should belong_to(:region).class_name('Region') }
-
     it { should belong_to(:resource_category).class_name('ResourceCategory') }
-
     it { should belong_to(:organization).class_name('Organization').optional }
   end
 end
