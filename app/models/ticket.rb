39f3d3d95e7e1535b6789de0f292d1eaa4c9dadc
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-19 21:36:35 -0800

    Document scopes with return type, remove spacing to match other models.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,11 +5,13 @@ class Ticket < ApplicationRecord
   belongs_to :resource_category
   belongs_to :organization, optional: true
 
+  # Validation
   validates_presence_of :name, :phone, :region_id, :resource_category_id
   validates_length_of :name, minimum: 1, maximum: 255, on: :create
   validates_length_of :description, maximum: 1020, on: :create
   validates :phone, phony_plausible: true
 
+  # Custom queries return ActiveRecord::Relation
   scope :open, -> () { where closed: false, organization_id: nil }
   scope :closed, -> () { where closed: true }
   scope :all_organization, -> () { where(closed: false).where.not(organization_id: nil) }
@@ -17,7 +19,6 @@ class Ticket < ApplicationRecord
   scope :closed_organization, -> (organization_id) { where(organization_id: organization_id, closed: true) }
   scope :region, -> (region_id) { where(region_id: region_id) }
   scope :resource_category, -> (resource_category_id) { where(resource_category_id: resource_category_id) }
-
 
   def open?
     !closed
