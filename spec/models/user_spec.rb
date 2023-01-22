ef0b80de26ef7b02db991d72453141d5724e3d19
Reese Ringo <ringor@oregonstate.edu>
2023-01-21 22:59:00 -0800

    User specs written

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+    let (:user) { User.new }
+
+    # Test instanciation
+
+    it "exists" do
+        user
+    end
+
+    # Test attributes
+
+    it "has a name" do
+        expect(user).to respond_to(:name)
+    end
+
+    it "has an email" do
+        expect(user).to respond_to(:email)
+    end
+
+    it "has a password" do
+        expect(user).to respond_to(:password)
+    end
+
+    # Test associations
+
+    context 'associations' do
+        it { should belong_to(:organization).class_name('Organization').optional }
+    end
+
 end
