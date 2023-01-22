9f1f163a663119c10fc81d34c27923b7c547a2da
Daltonshults <dshults@cocc.edu>
2023-01-22 11:31:21 -0800

    Added unspecified creation test.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,18 +3,27 @@ require 'rails_helper'
 RSpec.describe Region, type: :model do
 
   let(:region) {Region.create(name: "name")}
+  let(:region_unspecified) {Region.unspecified}
 
+  # Test instantiation
 
   it "exists" do
     region
   end
 
+  # Test attributes
+
   it "has a name" do
     expect(region).to respond_to(:name)
   end
 
+  # Testing functions
   it "has a string representation that is its name" do
     expect(region.to_s).to eq("name")
+  end
+
+  it "testing unspecified creation" do
+    expect(region_unspecified.to_s).to eq("Unspecified")
   end
 
 end
