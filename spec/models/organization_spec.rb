70f54ba53c759a606b9cdd4fdacda331ffd22a79
Daltonshults <dshults@cocc.edu>
2023-01-24 11:33:30 -0800

    Test validates presence of name.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -110,6 +110,10 @@ RSpec.describe Organization, type: :model do
     end
     
     # Validators
+    it "validates presence of name" do 
+      expect(organization).to validate_presence_of(:name)
+    end
+
     it "verifies the minimum length of name" do
       expect(organization).to validate_length_of(:name).is_at_least(1)
     end
