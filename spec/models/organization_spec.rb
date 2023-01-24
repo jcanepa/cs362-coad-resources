56f6358aa4e065c567ce2a436d97ed5ed4d3606f
Daltonshults <dshults@cocc.edu>
2023-01-24 11:26:55 -0800

    test validates the minimum length of name.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -108,4 +108,9 @@ RSpec.describe Organization, type: :model do
     context "associations: belongs to many resource categories" do
       it { should have_and_belong_to_many(:resource_categories).class_name("ResourceCategory")}
     end
+    
+    # Validators
+    it "verifies the minimum length of name" do
+      expect(organization).to validate_length_of(:name).is_at_least(1)
+    end
 end
