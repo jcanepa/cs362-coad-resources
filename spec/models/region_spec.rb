4b6432e80d5a7d0cd279f3fa35fb7df3b4a9d991
Daltonshults <dshults@cocc.edu>
2023-01-24 17:40:12 -0800

    Reformatted tests for legibility.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -19,36 +19,43 @@ RSpec.describe Region, type: :model do
 
   # Testing functions
   it "has a string representation that is its name" do
-    expect(region.to_s).to eq("name")
+    expect(region.to_s).to \
+    eq("name")
   end
 
   it "testing unspecified creation" do
-    expect(region_unspecified.to_s).to eq("Unspecified")
+    expect(region_unspecified.to_s).to \
+    eq("Unspecified")
   end
 
   it "validates presence of name" do
-    expect(region).to validate_presence_of(:name)
+    expect(region).to \
+    validate_presence_of(:name)
   end
 
   it "validates the minimum length of name" do
-    expect(region).to validate_length_of(:name)
+    expect(region).to \
+    validate_length_of(:name)
     .is_at_least(1)
     .on(:create)
   end
 
   it "validates the maximum length of name" do 
-    expect(region).to validate_length_of(:name)
+    expect(region).to \
+    validate_length_of(:name)
     .is_at_most(255)
     .on(:create)
   end
 
   it "validates the uniqueness of a name" do 
-    expect(region).to validate_uniqueness_of(:name)
+    expect(region).to \
+    validate_uniqueness_of(:name)
     .ignoring_case_sensitivity
   end
 
   it "validates unspecified static method" do
-    expect(Region.unspecified.name).to eq("Unspecified")
+    expect(Region.unspecified.name).to \
+    eq("Unspecified")
   end 
 
 end
