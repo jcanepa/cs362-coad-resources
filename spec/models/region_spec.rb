90c76da82af85c6bfffabff1744a41c81559ff39
Daltonshults <dshults@cocc.edu>
2023-01-24 17:09:00 -0800

    Added test to check minimum length of name, and removed Region.create(..) and replaced with Region.new(...).

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-  let(:region) {Region.create(name: "name")}
+  let(:region) {Region.new(name: "name")}
   let(:region_unspecified) {Region.unspecified}
 
   # Test instantiation
@@ -28,6 +28,10 @@ RSpec.describe Region, type: :model do
 
   it "validates presence of name" do
     expect(region).to validate_presence_of(:name)
+  end
+
+  it "validates the minimum length of name" do
+    expect(region).to validate_length_of(:name).is_at_least(1)
   end
 
 end
