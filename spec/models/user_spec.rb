be3b77436db6633c5342675786e7d4405e6eb097
Reese Ringo <ringor@oregonstate.edu>
2023-01-26 11:16:18 -0800

    Tests added to user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-    let (:user) { User.new }
+    let (:user) { 
+        User.new(
+
+            email: "email@email.com",
+            password: "password",
+            role: 1
+
+    ) }
 
     # Test instantiation
 
@@ -13,21 +20,30 @@ RSpec.describe User, type: :model do
     # Test attributes
 
     it "has an email" do
-        expect(user).to respond_to(:email)
+        expect(user)
+        .to respond_to(:email)
     end
 
     it "has a password" do
-        expect(user).to respond_to(:password)
+        expect(user)
+        .to respond_to(:password)
+    end
+
+    it "has a role" do
+        expect(user)
+        .to respond_to(:role)
     end
 
     # Test functions
 
     it "can set default role" do
-        expect(user).to respond_to(:set_default_role)
+        expect(user)
+        .to respond_to(:set_default_role)
     end
 
     it "can print email to string" do
-        expect(user).to respond_to(:to_s)
+        expect(user)
+        .to respond_to(:to_s)
     end
 
     # Test associations
@@ -39,23 +55,53 @@ RSpec.describe User, type: :model do
     # Test Validators
 
     it "validates email presence" do
-        expect(user).to validate_presence_of(:email)
+        expect(user)
+        .to validate_presence_of(:email)
     end
 
-    it "validates email length" do
-        expect(user).to validate_length_of(:email)
+    
+    it "verifies the minimum length of name" do
+        expect(user)
+        .to validate_length_of(:email)
+        .is_at_least(1)
+        .on(:create)
+    end
+
+    it "verifies the maximum length of name" do
+        expect(user)
+        .to validate_length_of(:email)
+        .is_at_most(255)
+        .on(:create)
     end
 
     it "validates email uniqueness" do
-        expect(user).to validate_uniqueness_of(:email)
+        expect(user)
+        .to validate_uniqueness_of(:email)
+        .ignoring_case_sensitivity
+    end
+
+    it "validates email structure (regex)" do
+        
+        #expect()
     end
 
     it "validates password presence" do
-        expect(user).to validate_presence_of(:password)
+        expect(user)
+        .to validate_presence_of(:password)
     end
 
-    it "validates password length" do
-        expect(user).to validate_length_of(:password)
+    it "validates the minimum length of name" do
+        expect(user)
+        .to validate_length_of(:password)
+        .is_at_least(7)
+        .on(:create)
+    end
+
+    it "validates the maximum length of name" do
+        expect(user)
+        .to validate_length_of(:password)
+        .is_at_most(255)
+        .on(:create)
     end
 
 end
