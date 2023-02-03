8c9001b92b58d93438718f22f56adee2b05090f1
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-02 16:50:08 -0800

    Refactor region spec into contexts.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -5,57 +5,64 @@ RSpec.describe Region, type: :model do
   let(:region) {create(:region)}
   let(:region_unspecified) {Region.unspecified}
 
-  # Test instantiation
-
+  # Model instanciates
   it "exists" do
     region
   end
 
-  # Test attributes
-
-  it "has a name" do
-    expect(region).to respond_to(:name)
+  describe "attributes" do
+    it "has a name" do
+      expect(region).to respond_to(:name)
+    end
   end
 
-  # Testing functions
-  it "has a string representation that is its name" do
-    expect(region.to_s).to \
-    eq("name")
+  # Test model methods (member functions)
+  describe "methods" do
+
+    it "has a string representation that is its name" do
+      expect(region.to_s).to \
+      eq("name")
+    end
+
+    it "testing unspecified creation" do
+      expect(region_unspecified.to_s).to \
+      eq("Unspecified")
+    end
   end
 
-  it "testing unspecified creation" do
-    expect(region_unspecified.to_s).to \
-    eq("Unspecified")
-  end
 
-  it "validates presence of name" do
-    expect(region).to \
-    validate_presence_of(:name)
-  end
+  # Test model validation rules
+  describe 'validation' do
 
-  it "validates the minimum length of name" do
-    expect(region).to \
-    validate_length_of(:name)
-    .is_at_least(1)
-    .on(:create)
-  end
+    it "validates presence of name" do
+      expect(region).to \
+      validate_presence_of(:name)
+    end
 
-  it "validates the maximum length of name" do
-    expect(region).to \
-    validate_length_of(:name)
-    .is_at_most(255)
-    .on(:create)
-  end
+    it "validates the minimum length of name" do
+      expect(region).to \
+      validate_length_of(:name)
+      .is_at_least(1)
+      .on(:create)
+    end
 
-  it "validates the uniqueness of a name" do
-    expect(region).to \
-    validate_uniqueness_of(:name)
-    .ignoring_case_sensitivity
-  end
+    it "validates the maximum length of name" do
+      expect(region).to \
+      validate_length_of(:name)
+      .is_at_most(255)
+      .on(:create)
+    end
 
-  it "validates unspecified static method" do
-    expect(Region.unspecified.name).to \
-    eq("Unspecified")
+    it "validates the uniqueness of a name" do
+      expect(region).to \
+      validate_uniqueness_of(:name)
+      .ignoring_case_sensitivity
+    end
+
+    it "validates unspecified static method" do
+      expect(Region.unspecified.name).to \
+      eq("Unspecified")
+    end
   end
 
 end
