4aa96562492969d4162b683daabc20f833574bea
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-09 11:29:21 -0800

    Passing first tickets controller spec.

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  let (:user) { create(:user, email: "email@email.com", password: "password", role: 1) }
 
+  describe 'GET ticket index' do
+    it { expect(get(:new)).to be_successful }
+  end
 end
