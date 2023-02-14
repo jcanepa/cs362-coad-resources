fa5ebe3113c7a72f2211c23de85378413b85d242
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-14 10:36:12 -0800

    Fix indentation on ticket controller spec.

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe TicketsController, type: :controller do
   let(:ticket) { create(:ticket) }
   let(:organization_approved_user) { create(:user, :organization_approved) }
   let(:organization_unapproved_user) { create(:user, :organization_unapproved) }
-  let (:user) { create(:user, email: "foo@example.com", password: "password", role: 0) }
-  let (:admin) { create(:user, email: "admin@example.com", password: "password", role: 1) }
+  let(:user) { create(:user, email: "foo@example.com", password: "password", role: 0) }
+  let(:admin) { create(:user, email: "admin@example.com", password: "password", role: 1) }
 
 #   describe 'GET ticket index' do
 #     it { expect(get(:new)).to be_successful }
