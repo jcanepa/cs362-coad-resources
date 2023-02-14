5f95759c0d67d9ba7ad146de9a6f17659669c6c9
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-14 10:33:27 -0800

    Rename traits in gs

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
   let(:ticket) { create(:ticket) }
+  let(:organization_approved_user) { create(:user, :organization_approved) }
+  let(:organization_unapproved_user) { create(:user, :organization_unapproved) }
   let (:user) { create(:user, email: "foo@example.com", password: "password", role: 0) }
   let (:admin) { create(:user, email: "admin@example.com", password: "password", role: 1) }
 
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -6,13 +6,13 @@ FactoryBot.define do
         before(:create) { |user| user.skip_confirmation! }
 
         # user with an approved org
-        trait :organization_approved do
+        trait :organization_approved_user do
             role { :organization }
             organization_id { create(:organization, :approved).id }
         end
 
         # user with an unapproved org
-        trait :organization_unapproved do
+        trait :organization_unapproved_user do
             role { :organization }
             organization_id { create(:organization).id }
         end
