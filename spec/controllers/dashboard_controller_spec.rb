46946509b00f5c7c90399ce126af4aedd9bdee63
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-14 10:28:52 -0800

    Implement sequences in user factory, from lecture on introducing yet more testing abstractions.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+  # describe "GET #index while logged in" do
+  #   let(:user) { create(:user) }
 
+  #   it { expect(get(:index)).to_be_successful }
+  # end
+
+  # describe "GET #index while logged out" do
+  #   it {expect(get(:index)).to redirect_to(new_user_session_path)}
+  # end
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,3 +1,31 @@
 FactoryBot.define do
-    factory :user do; end
+    factory :user do
+        email # this will use the sequence
+        password { "secret" }
+
+        before(:create) { |user| user.skip_confirmation! }
+
+        # user with an approved org
+        trait :organization_approved do
+            role { :organization }
+            organization_id { create(:organization, :approved).id }
+        end
+
+        # user with an unapproved org
+        trait :organization_unapproved do
+            role { :organization }
+            organization_id { create(:organization).id }
+        end
+    end
 end
+
+FactoryBot.define do
+    # used when attribute doesnt have default in factory
+    sequence :email do |n|
+      "fake#{n}@factory.com"
+    end
+
+    sequence :name do |n|
+      "Fake-#{n}"
+    end
+  end
