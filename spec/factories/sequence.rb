ab3d86c98e2c6d54c3f5537962223fb208981b20
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-16 11:22:06 -0800

    Add doc block notes to sequence to demystify the magic.

--- spec/factories/sequence.rb
+++ spec/factories/sequence.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
-    # used when attribute doesnt have default in factory
+    # these attributes serve as the default for ALL factories with a matching name
+    # but is only used when attribute doesnt have default set in the model's factory
     sequence :email do |n|
       "fake#{n}@factory.com"
     end
