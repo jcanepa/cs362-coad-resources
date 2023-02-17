a2d0a1995aa3528910404b43c67a67bb61a676d3
Daltonshults <dshults@cocc.edu>
2023-02-17 15:44:18 -0800

    Adjusted :organization_approved -> :organization_approved_user.

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe OrganizationsController, type: :controller do
   #before(:each) {sign_in(organization_approved_user)}
 
   describe 'GET resources' do
-    let(:organization_approved_user) { create(:user, :organization_approved)}
+    let(:organization_approved_user) { create(:user, :organization_approved_user)}
     let(:organization) {create(:organization)}
     before(:each) {sign_in(organization_approved_user)}
     it { expect(get(:index)).to be_successful } end
 
     #before(:example) {org = organization}
-    let(:organization_approved_user) { create(:user, :organization_approved)}
+    let(:organization_approved_user) { create(:user, :organization_approved_user)}
     before(:each) {sign_in(organization_approved_user)}
     it 'updates an organization' do
 
@@ -22,7 +22,7 @@ RSpec.describe OrganizationsController, type: :controller do
       expect(put :update, params: { id: organization.id, organization: { name: "new_name", status: :approved } }).to redirect_to(organization_path(id: organization.id))
     end
 
-    let(:organization_approved_user) { create(:user, :organization_approved)}
+    let(:organization_approved_user) { create(:user, :organization_approved_user)}
     before(:each) {sign_in(organization_approved_user)}
     it 'fails to update an organization' do
 
