7593589ffb14c0108ff20d91ae6103c05ba28b4f
Daltonshults <dshults@cocc.edu>
2023-02-17 14:36:31 -0800

    Changed gemfile, and got tests for updating an organization in the organization controller.

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
+  #before(:each) {sign_in(organization_approved_user)}
+
+  describe 'GET resources' do
+    let(:organization_approved_user) { create(:user, :organization_approved)}
+    let(:organization) {create(:organization)}
+    before(:each) {sign_in(organization_approved_user)}
+    it { expect(get(:index)).to be_successful } end
+
+    #before(:example) {org = organization}
+    let(:organization_approved_user) { create(:user, :organization_approved)}
+    before(:each) {sign_in(organization_approved_user)}
+    it 'updates an organization' do
+
+      organization = create(:organization, name: 'Old name')
+
+      organization.reload
+
+      expect(put :update, params: { id: organization.id, organization: { name: "new_name", status: :approved } }).to redirect_to(organization_path(id: organization.id))
+    end
+
+    let(:organization_approved_user) { create(:user, :organization_approved)}
+    before(:each) {sign_in(organization_approved_user)}
+    it 'fails to update an organization' do
+
+      organization = create(:organization, name: 'Old name', status: :approved)
+
+      organization.reload
+
+      expect(put :update, params: { id: organization.id, organization: { name: "", status: :rejected } }).to render_template(:edit)
+    end
+
 end
--- spec/factories/organization_factory.rb
+++ spec/factories/organization_factory.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :organization, class: "Organization" do
+    name
+    email
+    phone {"555-555-5555"}
+    primary_name {"primary_name"}
+    secondary_name {"secondary_name"}
+    secondary_phone {"666-666-6666"}
   end
 end
