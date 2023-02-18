d7b01ba4b998b4ff56ca566880c8de958f9c7966
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-18 10:53:41 -0800

    Passing ticket controller specs, moved into user types.

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,27 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
-  let(:ticket) { create(:ticket) }
-
-  let(:user) { create(:user) }
-  let(:admin) { create(:user, :admin) }
-
-  let(:organization_approved_user) { create(:user, :organization_approved) }
-  let(:organization_unapproved_user) { create(:user, :organization_unapproved) }
 
   context 'as an unauthenticated user' do
-    describe 'GET #new' do
+      let(:user) { create(:user) }
+      describe 'GET #new' do
       it { expect(get(:new)).to be_successful }
     end
   end
 
   context 'as an authenticated user' do
-    # describe 'GET #create' do
-    #   it { expect(get(:create)).to be_successful }
-    # end
+    describe 'GET #new' do
+      it { expect(get(:new)).to be_successful }
+    end
+
+    context 'who is part of an approved organization' do
+      let(:organization_approved_user) { create(:user, :organization_approved) }
+    end
+
+    context 'who is part of an unapproved organization' do
+      let(:organization_unapproved_user) { create(:user, :organization_unapproved) }
+    end
   end
 
   context 'as an authenticated admin user' do
+    let(:admin) { create(:user, :admin) }
+    describe 'GET #new' do
+      it { expect(get(:new)).to be_successful }
+    end
     # describe 'GET #show' do
     #   it { expect(get(:show)).to be_successful }
     # end
