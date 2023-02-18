302ab8dc5985e2e91433d4e313763a4118a2e8b3
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-18 15:14:53 -0800

    Tickets controller route testing.

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -3,16 +3,28 @@ require 'rails_helper'
 RSpec.describe TicketsController, type: :controller do
 
   context 'as an unauthenticated user' do
-      let(:user) { create(:user) }
-      describe 'GET #new' do
-      it { expect(get(:new)).to be_successful }
-    end
-  end
-
-  context 'as an authenticated user' do
+    # GET /tickets/new
     describe 'GET #new' do
       it { expect(get(:new)).to be_successful }
     end
+
+    # POST /tickets
+    describe 'GET #create' do
+        it { expect(get(:create)).to be_successful }
+    end
+
+    # describe 'GET #new' do
+    # it { expect(get(:new)).to be_successful }
+    # end
+
+    # describe 'GET #new' do
+    # it { expect(get(:new)).to be_successful }
+    # end
+  end
+
+  context 'as an authenticated user' do
+    let(:user) { create(:user) }
+    before(:each) { sign_in(user) }
 
     context 'who is part of an approved organization' do
       let(:organization_approved_user) { create(:user, :organization_approved) }
@@ -25,9 +37,7 @@ RSpec.describe TicketsController, type: :controller do
 
   context 'as an authenticated admin user' do
     let(:admin) { create(:user, :admin) }
-    describe 'GET #new' do
-      it { expect(get(:new)).to be_successful }
-    end
+
     # describe 'GET #show' do
     #   it { expect(get(:show)).to be_successful }
     # end
