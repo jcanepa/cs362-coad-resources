f3dbe1e4ce5603e0f474a39c3aa4a77b50a3bc43
Daltonshults <dshults@cocc.edu>
2023-02-19 22:09:05 -0800

    Created index tests for all three user types.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -10,4 +10,30 @@ RSpec.describe DashboardController, type: :controller do
   # describe "GET #index while logged out" do
   #   it {expect(get(:index)).to redirect_to(new_user_session_path)}
   # end
+
+  describe "approved logged in user" do
+    let(:approved_user) { create(:user, :organization_approved_user)}
+    before(:each) { sign_in(approved_user)}
+
+    it "checks index" do
+      expect(get(:index)).to be_successful
+    end
+  end
+
+  describe "logged in admin user" do
+    let(:approved_admin_user) { create(:user, :organization_approved_user, :admin)}
+    before(:each) { sign_in(approved_admin_user)}
+    it { expect(get(:index)).to be_successful}
+  end
+
+  describe "not logged in user" do 
+    it{expect(get(:index)).to redirect_to("/users/sign_in")}
+  end
+
+  describe "no organization user" do
+    let(:no_organization_user) {create(:user, :no_organization_user)}
+    before(:each) { sign_in(no_organization_user)}
+
+    it { expect(get(:index)).to be_successful}
+  end
 end
