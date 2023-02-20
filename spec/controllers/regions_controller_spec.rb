2174f88baca20819e3e5369973dc638475100f8a
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-19 20:37:07 -0800

    Duplicate tests in region spec for regular authenticated users.

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -5,14 +5,15 @@ RSpec.describe RegionsController, type: :controller do
   let(:region) { create(:region) }
 
   context 'as an unauthenticated user' do
-    # all region routes redirect unauthenticated users to login
     let(:user) { create(:user) }
+
+    # TLDR; all region routes redirect unauthenticated users to login
 
     # GET /
     describe 'GET #index' do
       it {
         expect(get(:index))
-        .to redirect_to("/users/sign_in")
+        .to redirect_to('/users/sign_in')
       }
     end
 
@@ -82,21 +83,56 @@ RSpec.describe RegionsController, type: :controller do
       }
     end
 
+    # GET /regions/:id
     describe 'GET #show' do
       it {
-        expect(
-          get(
-            :show,
-            params: {id: region.id}))
-          .to redirect_to(dashboard_path)
+        post(
+          :show, params: {id: region.id})
+        expect(response).to redirect_to(dashboard_path)
       }
     end
 
+    # GET /regions/new
+    describe 'GET #new' do
+      it {
+        expect(get(:new)) .to redirect_to(dashboard_path)
+      }
+    end
+
+    # POST /regions
     describe "POST #create" do
       it {
         post(
           :create,
           params: { region: attributes_for(:region) })
+        expect(response).to redirect_to(dashboard_path)
+      }
+    end
+
+    # GET /regions/:id/edit
+    describe 'GET #edit' do
+      it {
+        expect(
+          get(
+            :edit, params: {id: region.id}))
+        .to redirect_to(dashboard_path)
+      }
+    end
+
+    # PATCH /regions/:id
+    describe 'PATCH #update' do
+      it {
+        patch(
+          :update, params: {id: region.id})
+        expect(response).to redirect_to(dashboard_path)
+      }
+    end
+
+    # DELETE /regions/:id
+    describe 'PATCH #update' do
+      it {
+        delete(
+          :update, params: {id: region.id})
         expect(response).to redirect_to(dashboard_path)
       }
     end
