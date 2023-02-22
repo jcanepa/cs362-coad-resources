bf2147983726d8e4e7a76240528ef2fe7726a684
Reese Ringo <ringor@oregonstate.edu>
2023-02-21 22:32:54 -0800

    Added some resource category controller tests

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
     
+    let(:resource_category) {create(:resource_category)}
+
     context "as an unauthenticated user" do
 
         describe "testing index with unauthenticated user" do
@@ -21,6 +23,30 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
         describe "testing create with unauthenticated user" do
             it {expect(post(:create)).to redirect_to("/users/sign_in")}
+        end
+
+        describe "testing edit with unauthenticated user" do
+            it {
+                resources = create(:resource_category, name: "name")
+                expect(get :edit, params: {id: resources.id}).to redirect_to("/users/sign_in")
+            }
+        end
+#
+        describe "testing update with unauthenticated user" do
+            it {
+                patch(
+                    :update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+                expect(response).to redirect_to("/users/sign_in")
+            }
+        end
+#
+
+        describe "testing activate with unauthenticated user" do
+            it {
+                patch(
+                    :activate, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+                expect(response).to redirect_to("/users/sign_in")
+            }
         end
 
     end
@@ -48,6 +74,29 @@ RSpec.describe ResourceCategoriesController, type: :controller do
             it {expect(post(:create)).to redirect_to(dashboard_path)}
         end
 
+        describe "testing edit with authenticated user" do
+            it {
+                resources = create(:resource_category, name: "name")
+                expect(get :edit, params: {id: resources.id}).to redirect_to(dashboard_path)
+            }
+        end
+
+        describe "testing update with authenticated user" do
+            it {
+                patch(
+                    :update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+                expect(response).to redirect_to(dashboard_path)
+            }
+        end
+
+        describe "testing activate with authenticated user" do
+            it {
+                patch(
+                    :activate, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+                expect(response).to redirect_to(dashboard_path)
+            }
+        end
+
     end
 
     context "as an authenticated admin" do
@@ -72,30 +121,35 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
         describe "testing create with authenticated admin" do
             it {
-                #post :create, params: {resource_category: {name: "name"}}
                 post :create, params: {resource_category: attributes_for(:resource_category)}
                 expect(response).to redirect_to(resource_categories_path)
             }
         end
 
-        describe "testing successful activate with authenticated admin" do
+        describe "testing edit with authenticated admin" do
             it {
                 resources = create(:resource_category, name: "name")
-                resources.deactivate
-                patch :activate, params: {id: resources.id}
+                expect(get :edit, params: {id: resources.id}).to be_successful
+            }
+        end
+
+        describe "testing update with authenticated admin" do
+            it {
+                patch(
+                    :update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+                expect(response).to redirect_to(resource_category_path)
+                expect(flash[:notice]).to_not be_nil
+            }
+        end
+
+        describe "testing successful activate with authenticated admin" do
+            it {
+                patch :activate, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)}
                 expect(response).to redirect_to(resource_category_path)
                 expect(flash[:notice]).to_not be_nil
             }
         end
 
     end
-
-        
-    
-        
-    
-        
-    
-        
   
 end
