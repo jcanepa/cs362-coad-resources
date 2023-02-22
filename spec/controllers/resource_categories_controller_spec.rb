9a53063fb51899c34ac5226afe71188561b0f1cb
Reese Ringo <ringor@oregonstate.edu>
2023-02-21 19:53:06 -0800

    Some resource category controller tests

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -4,18 +4,48 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     
     context "as an unauthenticated user" do
 
-        describe "non logged in user " do
+        describe "testing index with unauthenticated user" do
             it {expect(get(:index)).to redirect_to("/users/sign_in")}
+        end
+
+        describe "testing show with unauthenticated user" do
+            it {
+                resources = create(:resource_category, name: "name")
+                expect(get :show, params: {id: resources.id}).to redirect_to("/users/sign_in")
+            }
+        end
+
+        describe "testing new with unauthenticated user" do
+            it {expect(get(:new)).to redirect_to("/users/sign_in")}
+        end
+
+        describe "testing create with unauthenticated user" do
+            it {expect(post(:create)).to redirect_to("/users/sign_in")}
         end
 
     end
 
     context "as an authenticated user" do
+        let(:user) {create(:user)}
+        before(:each) {sign_in(user)}
 
-        describe "logged in non admin user" do
-            let(:user) {create(:user)}
-            before(:each) {sign_in(user)}
-            it { expect(get(:index)).to redirect_to(dashboard_path)}
+        describe "testing index with authenticated user" do
+            it {expect(get(:index)).to redirect_to(dashboard_path)}
+        end
+
+        describe "testing show with authenticated user" do
+            it {
+                resources = create(:resource_category, name: "name")
+                expect(get :show, params: {id: resources.id}). to redirect_to(dashboard_path)
+            }
+        end
+
+        describe "testing new with authenticated user" do
+            it {expect(get(:new)).to redirect_to(dashboard_path)}
+        end
+
+        describe "testing create with authenticated user" do
+            it {expect(post(:create)).to redirect_to(dashboard_path)}
         end
 
     end
@@ -25,14 +55,37 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         let(:admin_approved_user) {create(:user, :admin)}
         before(:each) {sign_in(admin_approved_user)}
 
-        describe "testing index with logged in admin user" do     
+        describe "testing index with authenticated admin" do     
             it {expect(get(:index)).to be_successful}
         end
 
-        describe "testing show with logged in admin user" do        
+        describe "testing show with authenticated admin" do        
             it {
                 resources = create(:resource_category, name: "name")
-                expect(get :show, params: {id: resources.id}).to be_successful}
+                expect(get :show, params: {id: resources.id}).to be_successful
+            }
+        end
+
+        describe "testing new with authenticated admin" do
+            it {expect(get(:new)).to be_successful}
+        end
+
+        describe "testing create with authenticated admin" do
+            it {
+                #post :create, params: {resource_category: {name: "name"}}
+                post :create, params: {resource_category: attributes_for(:resource_category)}
+                expect(response).to redirect_to(resource_categories_path)
+            }
+        end
+
+        describe "testing successful activate with authenticated admin" do
+            it {
+                resources = create(:resource_category, name: "name")
+                resources.deactivate
+                patch :activate, params: {id: resources.id}
+                expect(response).to redirect_to(resource_category_path)
+                expect(flash[:notice]).to_not be_nil
+            }
         end
 
     end
--- spec/factories/resource_category_factory.rb
+++ spec/factories/resource_category_factory.rb
@@ -1,3 +1,5 @@
 FactoryBot.define do
-    factory :resource_category do; end
+    factory :resource_category do; 
+        name {"foo"}
+    end
 end
