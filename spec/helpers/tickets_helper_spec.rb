166c83ce5545a06a71b9d959375bde7bf8918e8d
Reese Ringo <ringor@oregonstate.edu>
2023-02-23 11:17:36 -0800

    Tickets_helper_spec at 100%

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -11,5 +11,17 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe TicketsHelper, type: :helper do
+    describe "can format phone number" do
+        it {
+            expect(helper.format_phone_number('+1 541-555-5555')).to eq '+15415555555'
+        }
 
+        it {
+            expect(helper.format_phone_number('+1 5415555555')).to eq '+15415555555'
+        }
+
+        it {
+            expect(helper.format_phone_number('5415555555')).to eq '+15415555555'
+        }
+    end
 end
