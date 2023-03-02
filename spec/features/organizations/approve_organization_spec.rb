7807ec3e3978bd6a45b7bb67e1cea905afcaca1c
Daltonshults <dshults@cocc.edu>
2023-03-02 11:06:21 -0800

    Checked admin, and non-admin users when approving an organization.

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  describe "admin approving an organization" do
+    it 'can be done from the dashboard' do
+      user = create(:user, :admin)
+      organization = create(:organization)
+      log_in_as(user)
 
+      visit dashboard_path
+
+      click_on('Organizations')
+      click_on('Review')
+      click_on('Approve')
+
+      expect(organization.reload.approved?).to be true
+
+    end
+
+    it "cannot be done by a non-admin user" do
+      organization = create(:organization, :approved)
+      not_approved_org = create(:organization)
+      user = create(:user, organization: organization)
+      log_in_as(user)
+
+      visit dashboard_path
+
+      #click_on "Organizations"
+
+      expect(page).to_not have_content 'Organizations Applications'
+      visit organization_path(id: not_approved_org.id)
+      expect(page).not_to have_content 'Approve'
+    end
+  end
 end
