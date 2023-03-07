89ecbc5dd8bfa6375b8f8204316d3e9caa31aba7
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-07 11:32:27 -0800

    First bit of org app feature passing, minor spacing update in tickets controller spec.

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
+  let(:user) { create(:user) }
+  before(:each) { log_in_as(user) }
 
+  describe 'it can foo' do
+    it {
+      visit dashboard_path
+
+      click_on('Create Application')
+      expect(page).to have_current_path('/new_organization_application')
+    }
+  end
 end
