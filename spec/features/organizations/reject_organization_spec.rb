d55968a98093cc5902e921b4069744412e3f1083
Daltonshults <dshults@cocc.edu>
2023-03-07 11:23:02 -0800

    Added reject and approve admin organizations, and added region creation w/ admin.

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+  describe "admin rejecting an organization" do
+    it "can be done from the dashboard" do 
+      user = create(:user, :admin)
+      organization = create(:organization)
+      log_in_as(user)
 
+      visit dashboard_path
+
+      click_on("Organization")
+      click_on("Pending")
+      click_on("Review")
+      click_on("Reject")
+
+      expect(organization.reload.approved?).to be false
+    end
+  end
 end
