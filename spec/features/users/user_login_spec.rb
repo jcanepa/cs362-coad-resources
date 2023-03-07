491c68e7b5f19331df21669bddf8dfd97819d4c9
Reese Ringo <ringor@oregonstate.edu>
2023-03-07 11:32:15 -0800

    User login feature under test

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+    describe "Normal user" do
+        it "can log in" do
+            user = create(:user, 
+                email: "email@email.com",
+                password: "password")
 
+            #visit dashboard_path
+
+            visit login_path
+
+            fill_in('Email address', with: user.email)
+            fill_in('Password', with: user.password)
+
+            find_by_id('commit').click
+            expect(current_path).to eq("/dashboard")
+        end
+    end
 end
