1a1bfcfc767726877af2ba296b87d3b13155be8f
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-07 11:51:16 -0800

    Start checking radio boxes.

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -4,12 +4,16 @@ RSpec.describe 'Creating an Organization Application', type: :feature do
   let(:user) { create(:user) }
   before(:each) { log_in_as(user) }
 
-  describe 'it can foo' do
+  describe 'new unaffiliated users can submit an organization application' do
     it {
       visit dashboard_path
 
       click_on('Create Application')
-      expect(page).to have_current_path('/new_organization_application')
+      expect(current_path).to eq('/new_organization_application')
+
+      # fill in the form
+      page.choose('organization_liability_insurance_true')
+      page.choose('organization_agreement_one_true')
     }
   end
 end
