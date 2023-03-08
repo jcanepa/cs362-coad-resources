585714b2e4a917eeddcf15e24c176d2c6dbc8d71
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-07 19:20:26 -0800

    Begin filling form for org app spec.

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -2,18 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
   let(:user) { create(:user) }
-  before(:each) { log_in_as(user) }
+  before(:each) {
+    log_in_as(user)
+    create(:resource_category)
+  }
 
-  describe 'new unaffiliated users can submit an organization application' do
+  describe 'unaffiliated users can submit organization app' do
     it {
-      visit dashboard_path
-
+      visit(dashboard_path)
       click_on('Create Application')
       expect(current_path).to eq('/new_organization_application')
 
       # fill in the form
-      page.choose('organization_liability_insurance_true')
-      page.choose('organization_agreement_one_true')
+
+      # fill_in('First Name', with: 'John')
+      # fill_in('Password', with: 'Seekrit')
+      # fill_in('Description', with: 'Really Long Text...')
+      # choose('A Radio Button')
+      # check('A Checkbox')
+      # uncheck('A Checkbox')
+      # attach_file('Image', '/path/to/image.jpg')
+      # select('Option', from: 'Select Box')
+
+      choose('organization_liability_insurance_true')
+      choose('organization_agreement_one_true')
+      check('organization_resource_category_ids_1')
+      fill_in 'organization_primary_name', :with => 'foo'
+
+      click_button('Apply')
     }
   end
 end
