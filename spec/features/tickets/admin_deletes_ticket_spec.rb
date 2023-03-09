839c0795f06c3a795fe9901a4c7499f31d734563
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-09 11:17:39 -0800

    Progress on admin deleted ticket spec.

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -4,9 +4,19 @@ RSpec.describe 'Deleting a Ticket', type: :feature do
   let(:admin) { create(:user, :admin) }
   let(:ticket) { create(:ticket) }
 
-  it 'admin user deletes an existing ticket' do
-    log_in_as(admin)
-    visit("tickets/#{ticket.id}")
-    expect(page).to have_content("Ticket #{ticket.id}")
+  describe 'admin user deletes an existing ticket', js: true do
+
+    it {
+      log_in_as(admin)
+      visit("tickets/#{ticket.id}")
+      expect(page).to have_content("Ticket #{ticket.id}")
+
+      accept_alert do
+        click_link('Delete')
+      end
+
+      expect(page).to have_current_path('/dashboard') # should be /dashboard#tickets but capybara doesn't like #anchors
+      expect(page).to have_content("Ticket #{ticket.id} was deleted.")
+    }
   end
 end
