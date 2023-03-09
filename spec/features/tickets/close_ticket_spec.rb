a88d4b233ca01180aa712ca38980b1b2b6907d3f
Reese Ringo <ringor@oregonstate.edu>
2023-03-09 11:36:23 -0800

    close_ticket_spec feature finished

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
+    let(:admin) {create(:user, :admin)}
+    let(:ticket) {create(:ticket)}
+
+    it "admin can close a ticket" do
+        #ticket = create(:ticket, name: 'Super Test')
+        log_in_as(admin)
+        visit("tickets/#{ticket.id}")
+        click_on("Close")
+        visit("tickets/#{ticket.id}")
+
+        expect(page).to have_content("Closed")
+    end
 
 end
