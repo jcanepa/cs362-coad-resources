8f658e49873092d14374cee7cf32a60165e00906
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-09 10:49:29 -0800

    Refactor string concat instead of str/int addition. Ticket factory working well.

--- spec/factories/ticket_factory.rb
+++ spec/factories/ticket_factory.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     name
     phone {5105555555}
     description {"Foo bar baz."}
+    region_id {create(:region).id}
+    resource_category_id {create(:resource_category).id}
 
     # an uncaptured ticket is not associated with an organization.
     trait :uncaptured do
--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'Deleting a Ticket', type: :feature do
   let(:admin) { create(:user, :admin) }
   let(:ticket) { create(:ticket) }
 
-  # it 'admin user deletes an existing ticket' do
-  #   log_in_as(admin)
-  #   visit('tickets/' + ticket.id)
-  #   expect(page).to have_content('Ticket' + ticket.id)
-  # end
+  it 'admin user deletes an existing ticket' do
+    log_in_as(admin)
+    visit("tickets/#{ticket.id}")
+    expect(page).to have_content("Ticket #{ticket.id}")
+  end
 end
