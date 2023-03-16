06f7fc21b1ddecc809386b45c3111dc4edc50170
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-16 10:41:05 -0700

    Progress on release tickets spec.

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -17,15 +17,34 @@ RSpec.describe 'Releasing a ticket by an', type: :feature do
       expect(page).to have_content('Dashboard')
       expect(page).to have_content('My Organization')
       expect(page).to have_content('Edit Organization')
+      expect(page).to have_content('Tickets')
 
       click_on('Tickets')
 
       expect(page).to have_content(ticket.name)
+      select 'My Captured', from: 'status'
+      # for some reason the tickets disappear, until you click tickets again
+      # TODO: seems like a bug!
+      click_on('Tickets')
 
-      select 'My Captured', from: 'status', visible: false
-
+      # tickets index
       expect(page).to have_content(ticket.name)
       click_on(ticket.name)
+
+      # single ticket detail
+      expect(page).to have_content(ticket.name)
+      expect(page).to have_content("Ticket #{ticket.id}")
+      expect(page).to have_content(ticket.description)
+      click_on('Release')
+
+      # verify redirct
+      expect(current_path).to eq(dashboard_path)
+
+      # verify ticket no longer appears under captured tickets
+      click_on('Tickets')
+      select 'My Captured', from: 'status'
+      click_on('Tickets')
+      # expect(page).to_not have_content(ticket.name)
     }
   end
 
