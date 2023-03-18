81f83be7aa7ff1cf2ecfb340e23a1fed7343e372
Daltonshults <dshults@cocc.edu>
2023-03-18 12:01:08 -0700

    Added class description.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
+# This is an abstract class that extends ActiveRecord::Base,
+# and will be used to instantiate applications for disaster
+# resources
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
